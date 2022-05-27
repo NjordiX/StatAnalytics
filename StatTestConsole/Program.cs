bdcdccecc92f79e63b176d12c05bc49dee557466
NjordiX <Njordix@outlook.com>
2022-05-27 12:44:45 +0300

    Подключение MaterialDesign и создание тестового консольного приложения

---
+++ StatTestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+namespace StatTestConsole
+{
+    internal class Program
+    {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
